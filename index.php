02818995743da523bb5f60cd9326a013cedea3ad
peter020104 <100434449+peter020104@users.noreply.github.com>
2022-02-26 01:40:26 +0800

    HW - Week 1 Exercise

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head></head>
+
+    <body></body>
+
+    <?php
+
+    $i=10;
+    $j=20;
+
+    echo "Hello <h1>PHP</h1>!";
+    echo "<br/>";
+    echo $i+$j;
+    echo "<br/>";
+    echo date("Y/m/d");
+
+    ?>
+
+</html>
